8e9026410e07f320fd39ad0764bdd7bd3d56762c
hiallyson <36341805+hiallyson@users.noreply.github.com>
2018-03-19 16:16:59 -0400

    Add files via upload

---
+++ sandeepvariableexercise.js
@@ -0,0 +1,25 @@
+// sandeep variable assignment -- Allyson Escobar, March 19 2018
+
+// A refugee inputs the number of members in their family.
+// Based on that number, you tell them how much money they will receive as assistance based on the following: 
+//  Each family member is given a one-time grant of $1,000 in 2018. Refugee families arriving in 2019 might get more or less depending on the State Department funding priorities. 
+// You need to know if the head of household speaks English. The refugee (and their family members) will be settled in the New York City where there are 5 NGOs that might help them. 
+// The five NGOs are "Help Unlimited", "New Start", "Better Tomorrow", "Home Safe" and "New Home". 
+// Monthly estimated expenses: $800 for one person, $950 for a couple, $1000 for a family of three, $1100 family of four, $1200 for family of 5+. 
+
+var familyNumber = x; // calculate number in family
+var onetimegrant = x * 1000; // 
+var englishSpeaking = y; // Does the head of household speak English? 
+var NGOs = " 'NewStart', "; // listing the NGOs   
+var NGOs = " 'HelpUnlimited', ";
+var NGOs = " 'BetterTomorrow', "; 
+var NGOs = " 'NewHome', "; 
+var NGOs = " 'HomeSafe', "; 
+var monthlyExpenses1 = 800; // family of one 
+var monthlyExpenses2 = 950; // family of two 
+var monthlyExpenses3 = 1000; // family of three 
+var monthlyExpenses4 = 1100; // family of four
+var monthlyExpenses5 = 1200; // family of five and up 
+
+
+// Honestly not sure what I'm doing, but here is an attempt! // 
